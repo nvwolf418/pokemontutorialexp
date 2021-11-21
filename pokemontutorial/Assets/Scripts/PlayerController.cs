c1e6d6b195fa972f0010f0d3e9d3190f4eeaa993
nvwolf418 <taispringer@gmail.com>
2021-11-21 06:59:49 -0500

    move should be finished, broken
    broken move, looks to be right, but is looping move

--- pokemontutorial/Assets/Scripts/PlayerController.cs
+++ pokemontutorial/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float moveSpeed;
+    private bool isMoving;
+
+    private Vector2 input;
+
+    public void Update()
     {
-        
+        if (!isMoving)
+        {
+            input.x = Input.GetAxisRaw("Horizontal");
+            input.y = Input.GetAxisRaw("Vertical");
+        }
+
+        if (input != Vector2.zero)
+        {
+            var targetPos = transform.position;
+            targetPos.x += input.x;
+            targetPos.y += input.y;
+            Debug.Log($"input x :y -> {input.x} :  {input.y}");
+            StartCoroutine(Move(targetPos));
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator Move(Vector3 targetPos)
     {
-        
+        isMoving = true;
+
+        while((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+            yield return null;
+        }
+
+        transform.position = targetPos;
+
+        isMoving = false;
     }
 }
