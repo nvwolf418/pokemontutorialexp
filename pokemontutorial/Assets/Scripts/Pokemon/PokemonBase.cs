8b8c92bf7a48775421e3d221eef2c1ebd065dd6f
nvwolf418 <taispringer@gmail.com>
2021-11-23 16:53:55 -0500

    base stats and pokemon base types added to the new pokemon base class, they are added as serializefields

---
+++ pokemontutorial/Assets/Scripts/Pokemon/PokemonBase.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PokemonBase : ScriptableObject
+{
+    // Start is called before the first frame update
+    [SerializeField] string name;
+
+    [TextArea][SerializeField] string description;
+    [SerializeField] Sprite frontSprite;
+    [SerializeField] Sprite backSprite;
+
+    [SerializeField] PokemonType type1;
+    [SerializeField] PokemonType type2;
+
+    //base stats
+    [SerializeField] int maxHp;
+    [SerializeField] int attack;
+    [SerializeField] int defense;
+    [SerializeField] int spAttack;
+    [SerializeField] int spDefense;
+    [SerializeField] int speed;
+}
+
+public enum PokemonType
+{
+    None,
+    Normal,
+    Fire,
+    Water,
+    Electric,
+    Grass,
+    Ice,
+    Fighting,
+    Poison,
+    Ground,
+    Flying,
+    Psychic,
+    Bug,
+    Rock,
+    Ghost,
+    Dragon
+}
