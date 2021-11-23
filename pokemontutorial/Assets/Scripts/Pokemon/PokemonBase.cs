5e60ff3e4ea544eb75f76f206b435c29491ee1b2
nvwolf418 <taispringer@gmail.com>
2021-11-23 18:05:12 -0500

    Set properties in pokemon class and pokemon base class.

--- pokemontutorial/Assets/Scripts/Pokemon/PokemonBase.cs
+++ pokemontutorial/Assets/Scripts/Pokemon/PokemonBase.cs
@@ -23,6 +23,67 @@ public class PokemonBase : ScriptableObject
     [SerializeField] int spAttack;
     [SerializeField] int spDefense;
     [SerializeField] int speed;
+
+    public string Name 
+    { 
+        get { return name; }
+    }
+
+
+    public string Description
+    {
+        get { return description; }
+    }
+
+    public Sprite FrontSprite
+    {
+        get { return frontSprite; }
+    }
+
+    public Sprite BackSprite
+    {
+        get { return backSprite; }
+    }
+
+    public PokemonType Type1
+    {
+        get { return type1; }
+    }
+
+    public PokemonType Type2
+    {
+        get { return type2; }
+    }
+
+    public int MaxHp
+    {
+        get { return maxHp; }
+    }
+
+    public int Attack
+    {
+        get { return attack; }
+    }
+
+    public int Defense
+    {
+        get { return defense; }
+    }
+
+    public int SpAttack
+    {
+        get { return spAttack; }
+    }
+
+    public int SpDefense
+    {
+        get { return spDefense; }
+    }
+
+    public int Speed
+    {
+        get {  return speed; }
+    }
 }
 
 public enum PokemonType
