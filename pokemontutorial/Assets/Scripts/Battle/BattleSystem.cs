f2def77fc256cef16cf4a9042dd65756247412e2
nvwolf418 <taispringer@gmail.com>
2021-11-24 16:19:00 -0500

    Enemy /player units and huds set up and

---
+++ pokemontutorial/Assets/Scripts/Battle/BattleSystem.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleSystem : MonoBehaviour
+{
+    [SerializeField] BattleUnit playerUnit;
+    [SerializeField] BattleUnit enemyUnit;
+    [SerializeField] BattleHud playerHud;
+    [SerializeField] BattleHud enemyHud;
+
+    private void Start()
+    {
+        SetUpBattle();
+    }
+
+    public void SetUpBattle()
+    {
+        playerUnit.Setup();
+        playerHud.SetData(playerUnit.Pokemon);
+        enemyUnit.Setup();
+        enemyHud.SetData(enemyUnit.Pokemon);
+    }
+}
--- pokemontutorial/Assets/Scripts/Pokemon/Pokemon.cs
+++ pokemontutorial/Assets/Scripts/Pokemon/Pokemon.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class Pokemon
 {
-    PokemonBase _base;
-    int level;
+    public PokemonBase Base { get; set; }
+    public int Level { get; set; }
     
     public int HP { get; set; }
 
@@ -13,14 +13,14 @@ public class Pokemon
 
     public Pokemon(PokemonBase pBase,int pLevel)
     {
-        _base = pBase;
-        level = pLevel;
-        HP = _base.MaxHp;
+        Base = pBase;
+        Level = pLevel;
+        HP = MaxHP;
 
         Moves = new List<Move>();
-        foreach (var move in _base.LearnableMoves)
+        foreach (var move in Base.LearnableMoves)
         {
-            if(move.Level <= level)
+            if(move.Level <= Level)
             {
                 Moves.Add(new Move(move.Base));
             }
@@ -34,31 +34,31 @@ public class Pokemon
 
     public int Attack
     {
-        get { return Mathf.FloorToInt(_base.Attack * level / 100f) + 5; }
+        get { return Mathf.FloorToInt(Base.Attack * Level / 100f) + 5; }
     }
 
     public int Defense
     {
-        get { return Mathf.FloorToInt(_base.Defense * level / 100f) + 5; }
+        get { return Mathf.FloorToInt(Base.Defense * Level / 100f) + 5; }
     }
 
     public int SpAttack
     {
-        get { return Mathf.FloorToInt(_base.SpAttack * level / 100f) + 5; }
+        get { return Mathf.FloorToInt(Base.SpAttack * Level / 100f) + 5; }
     }
 
     public int SpDefense
     {
-        get { return Mathf.FloorToInt(_base.SpDefense * level / 100f) + 5; }
+        get { return Mathf.FloorToInt(Base.SpDefense * Level / 100f) + 5; }
     }
 
     public int Speed
     {
-        get { return Mathf.FloorToInt(_base.Speed * level / 100f) + 5; }
+        get { return Mathf.FloorToInt(Base.Speed * Level / 100f) + 5; }
     }
 
     public int MaxHP
     {
-        get { return Mathf.FloorToInt(_base.Attack * level / 100f) + 10; }
+        get { return Mathf.FloorToInt(Base.Attack * Level / 100f) + 10; }
     }
 }
