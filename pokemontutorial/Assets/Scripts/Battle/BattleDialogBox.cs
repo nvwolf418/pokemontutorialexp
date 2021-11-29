ed676b40ca23dba336f5b53a7a377eb5788229cb
nvwolf418 <taispringer@gmail.com>
2021-11-28 21:58:58 -0500

    included acrtion state and move selection and details ui

---
+++ pokemontutorial/Assets/Scripts/Battle/BattleDialogBox.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BattleDialogBox : MonoBehaviour
+{
+    [SerializeField] int lettersPerSecond;
+    [SerializeField] Text dialogText;
+
+    //reference for action selector 
+    [SerializeField] GameObject actionSelector;
+    [SerializeField] GameObject moveSelector;
+    [SerializeField] GameObject moveDetails;
+
+    [SerializeField] List<Text> actionTexts;
+    [SerializeField] List<Text> moveTexts;
+
+    [SerializeField] Text ppText;
+    [SerializeField] Text typeText;
+
+    public void SetDialog(string Dialog)
+    {
+        dialogText.text = Dialog;
+    }
+
+    public IEnumerator TypeDialog(string dialog)
+    {
+        dialogText.text = "";
+        foreach (var letter in dialog.ToCharArray())
+        {
+            dialogText.text += letter;
+            yield return new WaitForSeconds(1f / lettersPerSecond);
+        }
+    }
+
+    public void EnableDialogText(bool enabled)
+    {
+        dialogText.enabled = enabled;
+    }
+
+    public void EnableActionSelector(bool enabled)
+    {
+        actionSelector.SetActive(enabled);
+    }
+
+    public void EnableMoveSelector(bool enabled)
+    {
+        moveSelector.SetActive(enabled);
+        moveDetails.SetActive(enabled);
+    }
+}
--- pokemontutorial/Assets/Scripts/Battle/BattleSystem.cs
+++ pokemontutorial/Assets/Scripts/Battle/BattleSystem.cs
@@ -2,23 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//variable to store state of battle
+public enum BattleState { Start, PlayerAction, PlayerMove, EnemyMove, busy}
 public class BattleSystem : MonoBehaviour
 {
     [SerializeField] BattleUnit playerUnit;
     [SerializeField] BattleUnit enemyUnit;
     [SerializeField] BattleHud playerHud;
     [SerializeField] BattleHud enemyHud;
+    [SerializeField] BattleDialogBox dialogBox;
+
+    BattleState state;
 
     private void Start()
     {
-        SetUpBattle();
+        StartCoroutine(SetUpBattle());
     }
 
-    public void SetUpBattle()
+    public IEnumerator SetUpBattle()
     {
         playerUnit.Setup();
         playerHud.SetData(playerUnit.Pokemon);
         enemyUnit.Setup();
         enemyHud.SetData(enemyUnit.Pokemon);
+        
+        
+        yield return(dialogBox.TypeDialog($"A wild {enemyUnit.Pokemon.Base.Name} appeared!"));
+        yield return new WaitForSeconds(1f);
+
+    }
+
+    void PlayerAction()
+    {
+        state = BattleState.PlayerAction;
+        StartCoroutine(dialogBox.TypeDialog("Choose an action"));
     }
 }
