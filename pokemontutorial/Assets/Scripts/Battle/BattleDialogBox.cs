2db0aece2363de5c1efb09c2546b04fcd913d58b
nvwolf418 <taispringer@gmail.com>
2021-11-29 22:25:19 -0500

    added menu move slection and menu option selection

--- pokemontutorial/Assets/Scripts/Battle/BattleDialogBox.cs
+++ pokemontutorial/Assets/Scripts/Battle/BattleDialogBox.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
 public class BattleDialogBox : MonoBehaviour
 {
     [SerializeField] int lettersPerSecond;
+    [SerializeField] Color highlightedColor;
     [SerializeField] Text dialogText;
 
     //reference for action selector 
@@ -48,5 +50,56 @@ public class BattleDialogBox : MonoBehaviour
     {
         moveSelector.SetActive(enabled);
         moveDetails.SetActive(enabled);
+    }
+
+    public void UpdateActionSelection(int selectionAction)
+    {
+        for(int i = 0; i < actionTexts.Count; i++)
+        {
+            if(i == selectionAction)
+            {
+                actionTexts[i].color = highlightedColor;
+            }
+            else
+            {
+                actionTexts[i].color = Color.black;
+            }
+        }
+    }
+
+    public void UpdateMoveSelection(int selectedMove, Move move)
+    {
+        for (int i = 0; i < moveTexts.Count;i++)
+        {
+            if (i == selectedMove)
+            {
+                moveTexts[i].color = highlightedColor;
+            }
+            else
+            {
+                moveTexts[i].color = Color.black;
+            }
+        }
+
+        ppText.text = $"PP {move.PP}/{move.Base.PP}";
+        typeText.text = move.Base.Type.ToString();
+    }
+
+
+
+
+    public void SetMoveNames(List<Move> moves)
+    {
+        for(int i = 0; i < moveTexts.Count; i++)
+        {
+            if(i < moves.Count)
+            {
+                moveTexts[i].text = moves[i].Base.Name;
+            }
+            else
+            {
+                moveTexts[i].text = ".";
+            }
+        }
     }
 }
--- pokemontutorial/Assets/Scripts/Battle/BattleSystem.cs
+++ pokemontutorial/Assets/Scripts/Battle/BattleSystem.cs
@@ -13,6 +13,8 @@ public class BattleSystem : MonoBehaviour
     [SerializeField] BattleDialogBox dialogBox;
 
     BattleState state;
+    int currentAction;
+    int currentMove;
 
     private void Start()
     {
@@ -25,16 +27,105 @@ public class BattleSystem : MonoBehaviour
         playerHud.SetData(playerUnit.Pokemon);
         enemyUnit.Setup();
         enemyHud.SetData(enemyUnit.Pokemon);
-        
-        
+
+        dialogBox.SetMoveNames(playerUnit.Pokemon.Moves);
+
         yield return(dialogBox.TypeDialog($"A wild {enemyUnit.Pokemon.Base.Name} appeared!"));
         yield return new WaitForSeconds(1f);
 
+
+        PlayerAction();
     }
 
     void PlayerAction()
     {
         state = BattleState.PlayerAction;
         StartCoroutine(dialogBox.TypeDialog("Choose an action"));
+        dialogBox.EnableActionSelector(true);
+    }
+
+    void PlayerMove()
+    {
+        state = BattleState.PlayerMove;
+        dialogBox.EnableActionSelector(false);
+        dialogBox.EnableDialogText(false);
+        dialogBox.EnableMoveSelector(true);
+    }
+
+    private void Update()
+    {
+        if(state == BattleState.PlayerAction)
+        {
+            HandleActionSelection();
+        }
+        else if(state == BattleState.PlayerMove)
+        {
+            HandleMoveSelection();
+        }
+    }
+
+    void HandleActionSelection()
+    {
+        if(Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if(currentAction < 1)
+            {
+                ++currentAction;
+            }
+        }
+        else if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if(currentAction > 0)
+            {
+                --currentAction;
+            }
+        }
+
+        dialogBox.UpdateActionSelection(currentAction);
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            if (currentAction == 0)
+            {
+                PlayerMove();
+            }
+            else if(currentAction == 1)
+            {
+                //run
+            }
+        }
+    }
+
+    void HandleMoveSelection()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if (currentMove < playerUnit.Pokemon.Moves.Count - 1)
+            {
+                ++currentMove;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if (currentMove > 0)
+            {
+                --currentMove;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if (currentMove < playerUnit.Pokemon.Moves.Count - 2)
+            {
+                currentMove += 2;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (currentMove > 0)
+            {
+                currentMove -= 2;
+            }
+        }
+        dialogBox.UpdateMoveSelection(currentMove, playerUnit.Pokemon.Moves[currentMove]);
     }
 }
